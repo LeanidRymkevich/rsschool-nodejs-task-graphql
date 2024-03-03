60464c99740b0f626af60935a2dbd05cb2f8e903
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-03-03 23:16:58 +0300

    feat: add depth limit check

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,10 +1,12 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { graphql } from 'graphql';
+import { GraphQLError, graphql, parse, validate } from 'graphql';
 
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import { gqlTypesSchema } from './schemas.js';
+import depthLimit from 'graphql-depth-limit';
 
 const UNKNOWN_ERR_MSG = 'Unknown error';
+const DEPTH_LIMIT = 5;
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -20,19 +22,23 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
     async handler(req) {
       const {query, variables} = req.body;
+      const errorArr: readonly GraphQLError[] = 
+        validate(gqlTypesSchema, parse(query), [depthLimit(DEPTH_LIMIT)]);
 
-       try {
+      if (errorArr.length > 0) return {errors: errorArr};
+
+      try {       
         return await graphql({
           schema: gqlTypesSchema,
           source: query,
           contextValue: { prisma },
-          variableValues: variables
+          variableValues: variables,
         });
-       } catch (err) {
+      } catch (err) {
         const msg = err instanceof Error ? (err.message || UNKNOWN_ERR_MSG) : UNKNOWN_ERR_MSG;
 
         return {errors: [msg]};
-       }
+      }
     },
   });
 };
